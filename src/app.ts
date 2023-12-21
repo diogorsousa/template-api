0d9e5190752dc06183bf46ff3f12bf09e4e7b77f
Diogo Sousa <46270931+diogorsousa@users.noreply.github.com>
2023-12-21 22:14:20 +0000

    Added logging for the api requests with daily file rotation Added 404 response

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,24 @@ import routes from './routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import { swaggerOptions } from './swaggerOptions';
+import morgan from 'morgan';
+import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+import path from 'path';
+
+const transport = new DailyRotateFile({
+  dirname: 'log',
+  filename: 'access-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '20m',
+  maxFiles: '14d'
+});
+
+const logger = winston.createLogger({
+  transports: [
+    transport
+  ]
+});
 
 const cors = require('cors');
 const app = express();
@@ -10,10 +28,18 @@ const app = express();
 const specs = swaggerJsdoc(swaggerOptions);
 app.use(cors());
 
+// setup the logger
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message) } }));
+
 // Swagger endpoint
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // All api routes are handled in routes.ts
 app.use('/api', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    res.status(404).json({ result: '404' });
+  });
 
 export default app;
